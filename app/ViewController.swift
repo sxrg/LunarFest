9ce8c6b677f310f99699e761007c2e957bb197e3
Yuni <yshuh0126@gmail.com>
2019-12-15 10:48:16 -0800

    Initial Commit

---
+++ app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Yunseong hur on 2019-12-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
