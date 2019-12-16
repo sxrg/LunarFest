698e1013f1ac4c01d797d5f71d86a42d93bfbc23
Sherry <sherryxrg@outlook.com>
2019-12-15 16:07:15 -0800

    created login page, added title font

---
+++ app/Login.swift
@@ -0,0 +1,28 @@
+//
+//  login.swift
+//  app
+//
+//  Created by Sherry Guo on 2019-12-15.
+//
+
+import UIKit
+
+class Login: UIViewController {
+    
+    @IBAction func signup(_ sender: UIButton) {
+        // connect with firebase
+    }
+    
+    @IBAction func login(_ sender: UIButton) {
+        // connect with firebase
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
