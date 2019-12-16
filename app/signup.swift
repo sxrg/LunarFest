14a213a3582b693bfea2852e120226af4f107a58
Yuni <yshuh0126@gmail.com>
2019-12-16 11:01:42 -0800

    fixed sign in error

--- app/signup.swift
+++ app/signup.swift
@@ -18,54 +18,71 @@ class signup: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var btnSubmit: UIButton!
     
-    private var token: String!
-    let defaults = UserDefaults.standard
+    var email: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        emailField.attributedPlaceholder = NSAttributedString(string: "", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
-        passwordField.attributedPlaceholder = NSAttributedString(string: "", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
-        
+        if Auth.auth().currentUser != nil{
+            //move to next view contorller
+        }else{
+        }
         setUpElements()
     }
     
     @IBAction func btnSignup(_ sender: Any) {
-        let myDatabse = Database.database().reference()
 
+    }
+    
+
+    //FIRAuthErrorCodeEmailAlreadyInUse
+    func createUser(){
         Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!){
-        (user, error) in
-        if error != nil {
-            let alert = UIAlertController(title: "ERROR", message: error?.localizedDescription, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert,animated: true, completion: nil)
-        } else if error == nil {
-            Auth.auth().currentUser?.sendEmailVerification{
-                (error) in
-                    if error != nil{
+            (authResult, error) in
+            if error != nil{
+                let errorCode = AuthErrorCode(rawValue: error!._code)
+                
+                if errorCode == .emailAlreadyInUse {
+                        //if email is already use then try signing in
+                        self.signIn()
+                    }else{
+                        //some kind of error that's not email has been used occured
                         let alert = UIAlertController(title: "ERROR", message: error?.localizedDescription, preferredStyle: .alert)
-                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                        self.present(alert,animated: true, completion: nil)
-                    } else {
-                        let user = Auth.auth().currentUser
-                            
-                        let alert = UIAlertController(title: "Verification Email Sent", message: "Please verfiy your email", preferredStyle: .alert)
-                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                        self.present(alert,animated: true, completion: nil)
-                        InstanceID.instanceID().instanceID{ (result, erorr) in if let error = error{
-                            print("error fetching")
-                    } else if let result = result {
-                            print("Remote instance id token: \(result.token)")
-                            self.token = result.token
-                            myDatabse.child("users/\(user!.uid)").setValue(["email": self.emailField.text])
-                            self.defaults.set(self.emailField.text, forKey: "email")
-                            }
-                        }
+                                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                                    self.present(alert,animated: true, completion: nil)
                     }
-                }
+            }else{
+                self.signIn()//directly signs in the user in after account is created
             }
         }
     }
+
+    func signIn(){//might not need to pass the two parameter in?
+
+         Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text!){
+                    (user, error) in
+                    if error != nil{
+                //cant sign in
+                        let alert = UIAlertController(title: "ERROR", message: error?.localizedDescription, preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(alert,animated: true, completion: nil)
+                        
+                    }else{
+                //signed in update user's email in database
+                        let user = Auth.auth().currentUser
+                        let uid = user!.uid
+                        let myDatabse = Database.database().reference()
+                        myDatabse.child("users").child(uid).child("email").setValue(user?.email)
+                        self.moveToLocationMenu()
+                        }
+                  }
+            }
+
+    func moveToLocationMenu(){
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let locationMenu = storyBoard.instantiateViewController(withIdentifier: "LocationMenu")
+            self.present(locationMenu, animated: true, completion: nil)
+    }
+        
 
         
 //    @IBAction func startLogin(_ sender: Any) {
