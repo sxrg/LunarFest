e94db0d74fb3c1225370def6f2f10a9d7d254050
sxrg <47671614+sxrg@users.noreply.github.com>
2020-10-28 17:39:26 -0700

    removed unused code

--- app/quiz.swift
+++ app/quiz.swift
@@ -61,7 +61,7 @@ class quiz: UIViewController {
     @IBAction func answerTapped(_ sender: UIButton) {
         if sender.tag == selectedAnswer {
             points += 10
-        dbRef.child("users").child(userID).child("point").setValue(points)
+            dbRef.child("users").child(userID).child("point").setValue(points)
             UserDefaults.standard.set(self.points, forKey: "points")
             UserDefaults.standard.set(lastDate, forKey:"lastQuizDate")
             
@@ -71,7 +71,7 @@ class quiz: UIViewController {
             
         } else {
             points += 5
-        dbRef.child("users").child(userID).child("point").setValue(points)
+            dbRef.child("users").child(userID).child("point").setValue(points)
             UserDefaults.standard.set(self.points, forKey: "points")
             UserDefaults.standard.set(lastDate, forKey:"lastQuizDate")
             
@@ -79,9 +79,6 @@ class quiz: UIViewController {
             
             moveToQuizEnd()
         }
-        
-        //questionNumber += 1
-        //updateQuestion()
         
     }
     
@@ -91,38 +88,6 @@ class quiz: UIViewController {
         quizend.modalPresentationStyle = .fullScreen
         self.present(quizend, animated: true, completion: nil)
     }
-    
-    // quiz refresh related functions -- not used
-//    func  updateQuestion() {
-//        if questionNumber <= allQuestions.list.count - 1{
-//            image_q.image = UIImage(named:(allQuestions.list[questionNumber].questionImage))
-//            question.text = allQuestions.list[questionNumber].question
-//            optA.setTitle(allQuestions.list[questionNumber].optionA, for: UIControl.State.normal)
-//            optB.setTitle(allQuestions.list[questionNumber].optionB, for: UIControl.State.normal)
-//            optC.setTitle(allQuestions.list[questionNumber].optionC, for: UIControl.State.normal)
-//            optD.setTitle(allQuestions.list[questionNumber].optionD, for: UIControl.State.normal)
-//            selectedAnswer = allQuestions.list[questionNumber].correctAnswer
-//            // updateUI()
-//
-//        } else {
-//            // should be replaced with action to go to page with quiz finished, how many points they got
-//            let alert = UIAlertController(title: "Quiz Finished!", message: "End of Quiz. Do you want to start over?", preferredStyle: .alert)
-//            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: {action in self.restartQuiz()})
-//            alert.addAction(restartAction)
-//            present(alert, animated: true, completion: nil)
-//        }
-//    }
-    
-//    func updateUI() {
-//        quizscore.text = "Score: \(score)"
-//        quizprogress.text = "\(questionNumber + 1)/\(allQuestions.list.count)"
-//    }
-    
-//    func restartQuiz() {
-//        score = 0
-//        questionNumber = 0
-//        updateQuestion()
-//    }
     
     // styling elements
     func setUpElements() {
